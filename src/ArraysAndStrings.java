fcb6378c20dc2e3f5e6d7c08daa8bc8e5f61c35d
Joe <jfranz@seas.upenn.edu>
2017-05-25 22:20:13 -0400

    first commit

---
+++ src/ArraysAndStrings.java
@@ -0,0 +1,13 @@
+/**
+ * Created by joseph on 5/25/17.
+ */
+public class ArraysAndStrings {
+
+    /**
+     * Method given a string find if it is unique
+     * @return boolean true or false
+     */
+    public boolean isUnique(){
+
+    }
+}
